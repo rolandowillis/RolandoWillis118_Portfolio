751ad39d53d49a72409631e5fc5e09f875ddf903
rolandowillis <rolandowillis118@gmail.com>
2022-06-08 16:40:28 +0900

    work experience tech erro fix

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -40,7 +40,7 @@ const exp = [
 const showCards2 = () => {
   let output = "";
   exp.forEach(
-    ({ title, cardImage, place, time, desp }) =>
+    ({ title, cardImage, place, tech, time, desp }) =>
       (output += `        
     <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
       <div class="card card1">
@@ -52,7 +52,7 @@ const showCards2 = () => {
             </div>
             <p class="meta">
               <span class="pre-heading">${place}</span><br>
-              <span class="author">${time}</span> <br>       
+              <span class="author">${time}</span><br>       
               <span class="tech">${tech}</span>    
             </p>
             <ol>
